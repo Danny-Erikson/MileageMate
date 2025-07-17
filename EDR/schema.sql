46db5ea6e2a276dd7a228e5d6f3403a4df6a94ff
Daniel Erikson <derikson413@gmail.com>
2025-07-17 11:14:20 -0700

    Updated Schema

--- EDR/schema.sql
+++ EDR/schema.sql
@@ -15,9 +15,43 @@ CREATE TABLE Users(
 
 CREATE TABLE Cars (
     carID INT NOT NULL AUTO_INCREMENT,
-    userID INT NOT NULL,
+    ownerID INT NOT NULL,
+    mechanicID INT,
     VINNumber CHAR(17) UNIQUE,
     licensePlate VARCHAR(10),
+    year INT,
+    make VARCHAR(15),
+    model VARCHAR(15),
+    trim VARCHAR(15),
+    imagePath VARCHAR(255),
     PRIMARY KEY (carID),
-    FOREIGN KEY (userID) REFERENCES Users(userID)
+    FOREIGN KEY (ownerID) REFERENCES Users(userID),
+    FOREIGN KEY (mechanicID) REFERENCES Users(userID)
+);
+
+CREATE TABLE Mileage (
+	mileageID INT NOT NULL AUTO_INCREMENT,
+    carID INT NOT NULL,
+    odometerReading INT NOT NULL,
+    readingDate DATE NOT NULL,
+    PRIMARY KEY (carID),
+    FOREIGN KEY (carID) REFERENCES cars(carID)
+);
+
+CREATE TABLE ReoccurringServices (
+	serviceID INT NOT NULL AUTO_INCREMENT,
+    serviceName VARCHAR(25),
+    carID INT,
+    dueMileage INT,
+    dueTimeValue INT,
+	dueTimeUnit ENUM('DAY', 'WEEK', 'MONTH', 'YEAR')
+);
+
+CREATE TABLE ServiceDone (
+	carServiceID INT NOT NULL AUTO_INCREMENT,
+    carID INT,
+    ServiceID INT,
+    MileageID INT,
+    ProductsUsed VARCHAR(1000),
+    Description VARCHAR(1000)
 );
