0415c11c684bfac39f6e0a1fe49795bacb0c0029
Daniel Erikson <derikson413@gmail.com>
2025-07-24 23:33:38 -0700

    Update CarView.js

--- src/components/CarView.js
+++ src/components/CarView.js
@@ -1,19 +1,4 @@
-// const vin = '1HGCM82633A004352';
-// fetch(
-//   `https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVinValuesExtended/${vin}?format=json`
-// )
-//   .then(response => response.json())
-//   .then(data => {
-//     const result = data.Results[0];
-//     let makeTitleCase = result.Make.charAt(0).toUpperCase() + result.Make.slice(1).toLowerCase();
-//     console.log(
-//       result.ModelYear,
-//       makeTitleCase,
-//       result.Model,
-//       result.Trim
-//     );
-//   })
-//   .catch(error => console.error('Error fetching VIN data:', error));
+
 
 
 //INSERT INTO ReoccurringServices
